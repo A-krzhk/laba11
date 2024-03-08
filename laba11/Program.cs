f90150e7cc0a25b3037326a40c9ac801026162b9
korya <korya@DESKTOP-5JP9O00>
2024-03-08 18:50:34 +0300

    Добавьте файлы проекта.

---
+++ laba11/Program.cs
@@ -0,0 +1,41 @@
+﻿using PlantLib;
+using System.Collections;
+namespace laba11
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Задание 1. 
+            //ArrayList
+
+            // 1. Создать универсальную коллекцию, добавить в нее объекты из созданной иерархии классов
+            ArrayList al = new ArrayList();
+            //Добавляем 5 цветков в конец массива
+            for (int i = 0; i < 5; i++)
+            {
+                Flower f = new Flower();
+                f.RandomInit();
+                al.Add(f);
+            }
+
+            //Добавляем 5 деревьев в конец массива
+            for (int i = 0; i < 5; i++)
+            {
+                Tree t = new Tree();
+                t.RandomInit();
+                al.Add(t);
+            }
+
+            al.Sort();
+
+            foreach (object p in al)
+            {
+                Console.WriteLine(p);
+            }
+
+
+        }
+    }
+}
